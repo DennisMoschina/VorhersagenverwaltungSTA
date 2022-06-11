2a6367a3ed125e459104b778a647ab6784e96bc8
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2022-06-11 21:35:13 +0200

    created project

---
+++ src/main/java/edu/kit/VorhersagenverwaltungSTA/VorhersagenverwaltungStaApplication.java
@@ -0,0 +1,13 @@
+package edu.kit.VorhersagenverwaltungSTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VorhersagenverwaltungStaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VorhersagenverwaltungStaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/kit/VorhersagenverwaltungSTA/VorhersagenverwaltungStaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kit.VorhersagenverwaltungSTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorhersagenverwaltungStaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
