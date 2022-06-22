059e9c7489f0833a6bf8fa1f02fa25e877a172c7
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2022-06-22 14:34:07 +0200

    added encoding of expand

--- src/main/java/edu/kit/VorhersagenverwaltungSTA/model/requestManager/encoder/SingleSelectionEncoder.java
+++ src/main/java/edu/kit/VorhersagenverwaltungSTA/model/requestManager/encoder/SingleSelectionEncoder.java
@@ -10,7 +10,7 @@ public class SingleSelectionEncoder extends SelectionEncoderTemplate {
     private static final String DATA_FORMAT = "%s(%d)?";
     @Override
     public String encode(Selection selection) {
-        if (!(selection instanceof final SingleSelection singleSelection))
+        if (!(selection instanceof SingleSelection))
             throw new IllegalArgumentException("the Selection has to be of type SingleSelection");
 
         return super.encode(selection);
@@ -18,13 +18,17 @@ public class SingleSelectionEncoder extends SelectionEncoderTemplate {
 
     @Override
     protected List<String> encodeStatements(Selection selection) {
-        return new LinkedList<>();
+        final List<String> encodedStatements = new LinkedList<>();
+
+        encodedStatements.add(new ExpandEncoder().encode(selection.getObjectsToExpand()));
+
+        return encodedStatements;
     }
 
     @Override
     protected String encodeHeader(Selection selection) {
         SingleSelection singleSelection = (SingleSelection) selection;
-        final String encodedType = new ObjectTypeEncoder().encode(singleSelection.getObjectType());
+        final String encodedType = new PluralObjectTypeEncoder().encode(singleSelection.getObjectType());
         return String.format(DATA_FORMAT, encodedType, singleSelection.getSelectedId());
     }
 }
