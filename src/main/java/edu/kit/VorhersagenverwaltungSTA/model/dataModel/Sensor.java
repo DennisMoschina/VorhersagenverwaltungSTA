de310b40bfd33482e0a90b0d82ea321ef956cb16
Elias Dirks <uqtfn@student.kit.edu>
2022-06-22 16:25:31 +0200

    added toString in Sensor

--- src/main/java/edu/kit/VorhersagenverwaltungSTA/model/dataModel/Sensor.java
+++ src/main/java/edu/kit/VorhersagenverwaltungSTA/model/dataModel/Sensor.java
@@ -13,8 +13,20 @@ public class Sensor {
     private String description;
     private String encodingType;
     private JsonNode metadata;
-
     private JsonNode properties;
+
+    @Override
+    public String toString() {
+        return "Sensor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", encodingType='" + encodingType + '\'' +
+                ", metadata=" + metadata +
+                ", properties=" + properties +
+                ", datastreamURL='" + datastreamURL + '\'' +
+                '}';
+    }
 
     @JsonProperty("Datastream@iot.navigationLink")
     private String datastreamURL;
