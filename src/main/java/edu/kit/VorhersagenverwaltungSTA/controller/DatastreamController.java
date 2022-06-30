c8fda3fa61e83311c5e3b975f3be707f9a03ac37
Elias Dirks <uqtfn@student.kit.edu>
2022-06-30 19:13:20 +0200

    added annotation for communication

--- src/main/java/edu/kit/VorhersagenverwaltungSTA/controller/DatastreamController.java
+++ src/main/java/edu/kit/VorhersagenverwaltungSTA/controller/DatastreamController.java
@@ -8,11 +8,13 @@ import edu.kit.VorhersagenverwaltungSTA.service.singleItem.DatastreamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 //@RequestMapping("/test")
 class DatastreamController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
